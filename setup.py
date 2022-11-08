a608e2e37621f0db422b76ee818f5197718719f7
Giorgi Khunashvili <giorgi.khunashvili.1@btu.edu.ge>
2022-11-08 07:20:34 +0400

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = ""
 EMAIL = "gkhunashvili@icloud.com"
 AUTHOR = "Giorgi Khunashvili"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 # Which packages are required for this module to be executed?
 REQUIRED = [
