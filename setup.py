a2a0829eae66a34eafc890918330b53baeacf32b
Giorgi Khunashvili <giorgi.khunashvili.1@btu.edu.ge>
2022-11-16 16:02:30 +0400

    updated package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "Lightweight library for developing real time applications"
 EMAIL = "gkhunashvili@icloud.com"
 AUTHOR = "Giorgi Khunashvili"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.0.11"
+VERSION = "0.0.12"
 
 # Which packages are required for this module to be executed?
 REQUIRED = [
